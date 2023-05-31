c0266d5b1120d95a6537871ccab47c6cc5f23f94
esteban-vm <estebanvm90@outlook.com>
2023-05-31 17:11:14 -0400

    initial config

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: { environment: 'jsdom', include: ['**/*.test.tsx'] },
+})
