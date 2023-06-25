eec47a098ac78ce90a8dd40470d590a012673177
esteban-vm <estebanvm90@outlook.com>
2023-06-25 17:41:09 -0400

    refactors on portfolio view

--- src/views/Portfolio/Portfolio.tsx
+++ src/views/Portfolio/Portfolio.tsx
@@ -8,26 +8,18 @@ const Portfolio: FC = () => {
   return (
     <Section bg='white' id='portfolio' subtitle='Lorem ipsum dolor sit amet' title='Our popular projects'>
       <div className={styles.wrapper}>
-        <div className={styles.column}>
-          {works.slice(0, 2).map((work) => (
-            <Work key={work.id} {...work} />
-          ))}
-        </div>
-        <div className={styles.column}>
-          {works.slice(2, 4).map((work) => (
-            <Work key={work.id} {...work} />
-          ))}
-        </div>
-        <div className={styles.column}>
-          {works.slice(4, 6).map((work) => (
-            <Work key={work.id} {...work} />
-          ))}
-        </div>
-        <div className={styles.column}>
-          {works.slice(6).map((work) => (
-            <Work key={work.id} {...work} />
-          ))}
-        </div>
+        {[...Array(4)].map((_, index) => {
+          const start = index * 2
+          const end = start + 2
+
+          return (
+            <div key={index} className={styles.column}>
+              {works.slice(start, end).map((work) => (
+                <Work key={work.id} {...work} />
+              ))}
+            </div>
+          )
+        })}
       </div>
     </Section>
   )
