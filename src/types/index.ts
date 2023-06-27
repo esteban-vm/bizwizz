437af7d937833e936b9f9a849158d55a612e2876
esteban-vm <estebanvm90@outlook.com>
2023-06-27 17:12:30 -0400

    refactors on types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
 export type { FC, PropsWithChildren } from 'react'
 export type { IconName } from '@/components'
-export type ID = ReturnType<typeof import('node:crypto').randomUUID>
+export type UUID = ReturnType<typeof import('node:crypto').randomUUID>
 export type ImageData<P extends string> = { src: `/images/${P}.jpg`; height: number; width: number }
